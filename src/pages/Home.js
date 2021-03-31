7521403d7e9879492830edb28507a256167ec4d7
Michael Coxe <mcoxe@me.com>
2021-03-30 21:39:27 -0800

    fixed some styling issues

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../context/shopContext';
-import { Box, Grid, Text, Image } from '@chakra-ui/react';
+import { Box, Grid, Text, Image, Center } from '@chakra-ui/react';
 
 const Home = () => {
 	const { fetchAllProducts, products } = useContext(ShopContext);
@@ -16,22 +16,27 @@ const Home = () => {
 		<div>
 			<Grid templateColumns={['repeat(1,1fr)', 'repeat(3,1fr)']}>
 				{products.map((product) => {
-					<Link to={`/products/${product.handle}`} key={product.id}>
-						<Box
-							_hover={{ opacity: '80%' }}
-							textAlign="center"
-							position="relative"
-						>
-							<Image src={product.images[0].src} />
-							<Text position="absolute" bottom="15%" w="100%" fontWeight="bold">
-								{product.title}
-							</Text>
-							<Text position="absolute" bottom="5%" w="100%" color="gray">
-								${product.variants[0].price}
-							</Text>
-						</Box>
-						;
-					</Link>;
+					return (
+						<Center>
+							<Link to={`/products/${product.handle}`} key={product.id}>
+								<Box
+									_hover={{ opacity: '80%' }}
+									textAlign="center"
+									position="block"
+									h="100%"
+								>
+									<Image src={product.images[0].src} margin="auto" />
+
+									<Text w="100%" fontWeight="bold">
+										{product.title}
+									</Text>
+									<Text w="100%" color="gray">
+										${product.variants[0].price}
+									</Text>
+								</Box>
+							</Link>
+						</Center>
+					);
 				})}
 			</Grid>
 		</div>
